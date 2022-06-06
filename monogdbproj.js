bbeeb4b5a1067a4e603e4ec3d61938fc69db3213
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-06-06 16:42:33 +0200

    mongo project file for code editing

---
+++ monogdbproj.js
@@ -0,0 +1,106 @@
+let users = ([
+    {
+      Username: "Abraham Cooper",
+      Password: "kwt@rt2022!",
+      Email: "Abraham.will@gmail.com",
+      Birthday: "26/04/1999",
+      FavoriteMovies: ["Star Wars:The Last Jedi",  "The Adam Project", "Spider-Man: No Way Home"],
+    },
+  
+    {
+      Username: "Emma W. Wyatt",
+      Password: "BwR$00V_!Afjq",
+      Email: "Wemmattawy35@gmail.com",
+      Birthday: "11/5/1996",
+      FavoriteMovies: ["Game of Thrones", "The Lord of the Rings: The Return of the King", "Eternals" ],
+    },
+
+    {
+        Username: "Matthew Johnson",
+        Password: "wil0026@1-#zybq%5T",
+        Email: "james.mont@gmail.com",
+        Birthday: "19/10/1967",
+        FavoriteMovies: ["Eternals", "Game of Thrones", "The Matrix"],
+      },
+
+      {
+        Username: "Jane E.Addison",
+        Password: "E29.wkpjr_my!47Km",
+        Email: "Ejan.alTw25@hotmail.com",
+        Birthday: "15/12/1992",
+        FavoriteMovies: ["Man of Steel", "Game of Thrones", "Spider-Man: No Way Home"],
+      },
+
+      {
+        Username: "Alexa Bright",
+        Password: "Ale_tr00!wm09g",
+        Email: "brightalexa.343t@gmail.com",
+        Birthday: "10/8/1974",
+        FavoriteMovies: ["The Matrix", "Star Wars:The Last Jedi", "Smallville", "Game of Thrones"],
+      },
+
+      {
+        Username: "Williams Smith",
+        Password: "Wspgkf0562g336488_23",
+        Email: "willsmith.8103@gmail.com",
+        Birthday: "23/2/1997",
+        FavoriteMovies: ["The Lord of the Rings: The Return of the King", "Eternals", "The Lord of the Rings: The Fellowship of the Ring"],
+      },
+  ]);
+
+
+
+
+
+
+
+
+  let users = ([
+    {
+      Username: "Abraham Cooper",
+      Password: "kwt@rt2022!",
+      Email: "Abraham.will@gmail.com",
+      Birthday: "26/04/1999",
+      FavoriteMovies: ["6290cad69cc37ae6f751ddc9", "6290cad69cc37ae6f751ddcb"],
+    },
+  
+    {
+      Username: "Emma W. Wyatt",
+      Password: "BwR$00V_!Afjq",
+      Email: "Wemmattawy35@gmail.com",
+      Birthday: "11/5/1996",
+      FavoriteMovies: ["6290cad69cc37ae6f751ddce", "6290cad69cc37ae6f751ddd1", ],
+    },
+
+    {
+        Username: "Matthew Johnson",
+        Password: "wil0026@1-#zybq%5T",
+        Email: "james.mont@gmail.com",
+        Birthday: "19/10/1967",
+        FavoriteMovies: ["6290cad69cc37ae6f751ddcc", "6290cad69cc37ae6f751ddca"],
+      },
+
+      {
+        Username: "Jane E.Addison",
+        Password: "E29.wkpjr_my!47Km",
+        Email: "Ejan.alTw25@hotmail.com",
+        Birthday: "15/12/1992",
+        FavoriteMovies: ["6290cad69cc37ae6f751ddc8", "6290cad69cc37ae6f751ddcb", "6290cad69cc37ae6f751ddcf"],
+      },
+
+      {
+        Username: "Alexa Bright",
+        Password: "Ale_tr00!wm09g",
+        Email: "brightalexa.343t@gmail.com",
+        Birthday: "10/8/1974",
+        FavoriteMovies: ["6290cad69cc37ae6f751ddc9", "6290cad69cc37ae6f751ddd0"],
+      },
+
+      {
+        Username: "Williams Smith",
+        Password: "Wspgkf0562g336488_23",
+        Email: "willsmith.8103@gmail.com",
+        Birthday: "23/2/1997",
+        FavoriteMovies: ["6290cad69cc37ae6f751ddcc", "6290cad69cc37ae6f751ddd1", "6290cad69cc37ae6f751ddca"],
+      },
+  ])
