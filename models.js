0ddd7aca031b7ee80ecd832f983b51acad2cd87f
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-06-08 21:15:46 +0200

    update to code

--- models.js
+++ models.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
@@ -21,11 +21,11 @@ let movieSchema = mongoose.Schema({
     Password: {type: String, required: true},
     Email: {type: String, required: true},
     Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
   });
   
-  let Movie = mongoose.model('Movie', movieSchema);
-  let User = mongoose.model('User', userSchema);
+  let Movie = mongoose.model("Movie", movieSchema);
+  let User = mongoose.model("User", userSchema);
   
   module.exports.Movie = Movie;
   module.exports.User = User;
