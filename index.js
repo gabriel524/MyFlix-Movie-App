707486a722c1fec8a8e80dda304fa44032391f71
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-12-06 01:02:48 +0100

    new update to code

--- index.js
+++ index.js
@@ -50,6 +50,31 @@ app.use((err, req, res, next) => {
   res.status(500).send("somethng seems broken here!");
 });
 
+const cors = require("cors");
+app.use(cors());
+
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://localhost:1234",
+  "http://localhost:4200",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        //If a specific origin isn't found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn't allow acces from origin" +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 //Begging of read
 app.get("/", (req, res) => {
   res.send("Welcome to my movies app!");
